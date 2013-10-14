82b575598a6bf53e24ff47d2ebcb87ad519cd34e
pbodilis <pierre.bodilis+github@gmail.com>
2013-10-14 14:33:01 +0200

    [code] fix header page, fix non image pages

--- single-default.php
+++ single-default.php
@@ -1,5 +1,22 @@
-<?php
-echo the_core()->render('photoblog_image', $post);
-echo the_core()->render('photoblog_content', $post);
+<div id="wrapper" class="">
+    <h2 id="content_title"><?php echo $post->title; ?></h2>
+    <section id="content" class="<?php echo $post->class; ?>">
+        <article class="post_content"><?php echo $post->content; ?></article>
+        <article class="post_categories">
+            <ul>
+                <?php foreach ($post->categories as $cat) { ?>
+                <li><a href="<?php echo $cat->url; ?>">&#91;<?php echo $cat->name; ?>&#93;</a></li>
+                <?php } ?>
+            </ul>
+        </article>
 
-?>
+        <?php if (isset($post->ratings)) { ?>
+        <article class="post_rating"> <?php echo $post->ratings; ?> </article>
+        <?php } ?>
+
+        <div class="clr"></div>
+    </section>
+
+    <?php comments_template(); ?>
+</div>
+
